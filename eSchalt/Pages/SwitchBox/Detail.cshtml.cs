1112f0fd53e6e7495a52da7872aabe44ef0d6e77
Dulak <110743178+BSCDulak@users.noreply.github.com>
2025-11-07 04:11:20 +0100

    Update Detail.cshtml.cs
    small fix to avoid issues with old SwitchBoxId cookies

--- eSchalt/Pages/SwitchBox/Detail.cshtml.cs
+++ eSchalt/Pages/SwitchBox/Detail.cshtml.cs
@@ -70,9 +70,20 @@ public class DetailpageModel : PageModel
                 UpdateImage();
                 return Page();
             }
+            else
+            {
+                // remove old cookie to prevent issues with old SwitchBox if the link couldn't be associated with a Switchbox in SwitchBoxQRLinks table
+                if (Request.Cookies.ContainsKey("SwitchBoxId"))
+                {
+                    Response.Cookies.Delete("SwitchBoxId");
+                }
+                return RedirectToPage("/Error/NoSwitchBox");
+            }
         }
 
-        // Fall back to cookie-based logic if no fileName provided
+        // Fall back to cookie-based logic if no fileName provided, e.g. if the QR-Code just directs to the details page with no filename provided.
+        // This means that someone can use their last cookie, this can prolly be useful for redirect shenanigans but might cause issues if an old cookie
+        // should have been removed instead.
         Initialize();
 
         if (SwitchBox == null)
