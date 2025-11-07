e625a6f8f9d34c2bcb460fd412390bd19a5cf136
Dulak <110743178+BSCDulak@users.noreply.github.com>
2025-11-07 01:04:53 +0100

    add Migration AddSwitchBoxQRLink

---
+++ eSchalt/Migrations/20251106235119_AddSwitchBoxQRLink.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace eSchalt.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSwitchBoxQRLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SwitchBoxQRLinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SwitchBoxId = table.Column<int>(type: "integer", nullable: false),
+                    QRLink = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SwitchBoxQRLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SwitchBoxQRLinks_SwitchBoxes_SwitchBoxId",
+                        column: x => x.SwitchBoxId,
+                        principalTable: "SwitchBoxes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "SwitchBoxQRLinks",
+                columns: new[] { "Id", "QRLink", "SwitchBoxId" },
+                values: new object[] { 1, "http://localhost:5000/detail?fileName=32fe4380-615b-4ed7-8622-a981303264dc.png", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SwitchBoxQRLinks_SwitchBoxId",
+                table: "SwitchBoxQRLinks",
+                column: "SwitchBoxId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SwitchBoxQRLinks");
+        }
+    }
+}
